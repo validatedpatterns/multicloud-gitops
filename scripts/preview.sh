27ffad5f258489eec9cc8a9b9577a10d9a7c02fa
Andrew Beekhof <andrew@beekhof.net>
2023-10-19 01:22:20 +0000

    Handle explcit value files

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -15,6 +15,16 @@ platform=$(oc get Infrastructure.config.openshift.io/cluster  -o jsonpath='{.spe
 ocpversion=$(oc get clusterversion/version -o jsonpath='{.status.desired.version}' | awk -F. '{print $1"."$2}')
 domain=$(oc get Ingress.config.openshift.io/cluster -o jsonpath='{.spec.domain}' | sed 's/^apps.//')
 
+function replaceGlobals() {
+    output=$( echo $1 | sed -e 's/ //g' -e 's/\$//g' -e s@^-@@g  -e s@\'@@g )
+
+    output=$(echo $output | sed "s@{{.Values.global.clusterPlatform}}@${platform}@g")
+    output=$(echo $output | sed "s@{{.Values.global.clusterVersion}}@${ocpversion}@g")
+    output=$(echo $output | sed "s@{{.Values.global.clusterDomain}}@${domain}@g")
+
+    echo $output
+}
+
 CLUSTER_OPTS=""
 CLUSTER_OPTS="$CLUSTER_OPTS --set global.pattern=$pattern"
 CLUSTER_OPTS="$CLUSTER_OPTS --set global.repoURL=$GIT_REPO"
@@ -28,4 +38,26 @@ CLUSTER_OPTS="$CLUSTER_OPTS --set global.clusterVersion=$ocpversion"
 CLUSTER_OPTS="$CLUSTER_OPTS --set global.clusterPlatform=$platform"
 
 
-helm template $chart --name-template ${APP} -n ${namespace} ${CLUSTER_OPTS}
+sharedValueFiles=$(yq ".clusterGroup.sharedValueFiles" values-$SITE.yaml)
+appValueFiles=$(yq ".clusterGroup.applications.$APP.extraValueFiles" values-$SITE.yaml)
+
+VALUE_FILES=""
+IFS=$'\n'
+for line in $sharedValueFiles; do
+    if [ $line != "null" ]; then
+	file=$(replaceGlobals $line)
+	VALUE_FILES="$VALUE_FILES -f $PWD$file"
+    fi
+done
+
+for line in $appValueFiles; do
+    if [ $line != "null" ]; then
+	file=$(replaceGlobals $line)
+	VALUE_FILES="$VALUE_FILES -f $PWD$file"
+    fi
+done
+
+cmd="helm template $chart --name-template ${APP} -n ${namespace} ${VALUE_FILES} ${CLUSTER_OPTS}"
+eval $cmd
+
+    
